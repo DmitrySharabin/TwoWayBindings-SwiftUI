4fc5757c061b36d943dcf590a1e11395a0bd87f7
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-01-02 22:50:51 +0100

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Dmitry Sharabin on 2.1.23..
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
